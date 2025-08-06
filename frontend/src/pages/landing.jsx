7fe0f15b8d48e5c65a43a9ae1db2207740e3ad07
Vivek Patil <vivekpatil9944@gmail.com>
2025-08-06 17:55:19 +0530

    Initial commit for Omnifocus project

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -10,7 +10,7 @@ export default function LandingPage() {
         <div className='landingPageContainer'>
             <nav>
                 <div className='navHeader'>
-                    <h2>Apna Video Call</h2>
+                    <h2>OmniFocus</h2>
                 </div>
                 <div className='navlist'>
                     <p onClick={() => {
@@ -32,13 +32,14 @@ export default function LandingPage() {
 
             <div className="landingMainContainer">
                 <div>
-                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+<h1 className="animated-heading"><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
 
-                    <p>Cover a distance by Apna Video Call</p>
+                    <p>Cover a distance by OmniFocus</p>
                     <div role='button'>
                         <Link to={"/auth"}>Get Started</Link>
                     </div>
                 </div>
+                
                 <div>
 
                     <img src="/mobile.png" alt="" />
